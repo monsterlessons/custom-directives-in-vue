ac88feac198d3f57c83e625f8ee4d7cb1b2df14f
Alexander Kochergin <ejiqpep@gmail.com>
2018-03-03 19:51:52 +0100

    Added forms in vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,47 @@
 <template>
   <div>
     <h1>Hello Vue!</h1>
-    <alert>
-      <div>
-        Top default slot
-      </div>
-      <div slot='title'>
-        <b>This is</b> {{message}}
-      </div>
-      <div slot='description'>
-        This is our description
-      </div>
-      <div>
-        Default slot bottom
-      </div>
-    </alert>
+
+    <div>
+      <label>Every day</label>
+      <input type='radio' value='everyDay' v-model='frequency' />
+    </div>
+    <div>
+      <label>Every week</label>
+      <input type='radio' value='oncePerWeek' v-model='frequency' />
+    </div>
+    <div>
+      <label>Every month</label>
+      <input type='radio' value='oncePerMonth' v-model='frequency' />
+    </div>
+    {{frequency}}
+
+    <div>
+      <select v-model='selectedCountry'>
+        <option v-for='country in countries'>
+          {{country}}
+        </option>
+      </select>
+      {{selectedCountry}}
+    </div>
   </div>
 
 </template>
 
 <script>
-import alert from './Alert.vue'
 export default {
   data () {
     return {
-      message: 'our success message'
+      frequency: 'oncePerMonth',
+      countries: [
+        'Austria',
+        'Sweden',
+        'Australia',
+        'Canada',
+        'Germany'
+      ],
+      selectedCountry: 'Germany'
     }
-  },
-  components: {
-    alert
   }
 }
 </script>
-
-<style scoped>
-  b {
-    color: red;
-  }
-</style>
