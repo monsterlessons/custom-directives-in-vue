0383b357bb782d4ff50e5d4ed9e2d1b6b53bfb73
Alexander Kochergin <ejiqpep@gmail.com>
2018-03-17 18:34:44 +0100

    Added custom directives lesson

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('success', {
+  bind (el, binding) {
+    el.style.color = binding.value.color
+    el.style.background = binding.value.background
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
